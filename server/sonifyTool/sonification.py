2c09240d6ccadafe8f6abb00219030f2f3032e37
evilditch <ronja@pahaoja.fi>
2022-09-17 11:14:05 +0300

    Äänen generointi eriytetty omaksi funktioksi

--- server/sonifyTool/sonification.py
+++ server/sonifyTool/sonification.py
@@ -6,24 +6,34 @@ class Sonification:
         self.x = np.linspace(0, 5, 100)
         self.y = np.sin(self.x)
         print(len(self.y))
-        self.fs = 48000 # samples per seconds
+        self.rate = 48000 # samples per seconds
         self.duration = 5 #total duration in seconds
         
-        t = np.linspace(0, self.duration, int(self.duration*self.fs))
-        frequences = np.repeat(self.pitches(), len(t)/len(self.y))
-        print(len(frequences), len(t))
+        # t = np.linspace(0, self.duration, int(self.duration*self.rate))
+        # frequences = np.repeat(self.pitches(), len(t)/len(self.y))
+        # print(len(frequences), len(t))
         
-        self.sample = np.sin(2 * np.pi * t * frequences)
+        # self.samples = np.sin(2 * np.pi * t * frequences)
+        self.samples = self.generateSamples()
         
     def pitches(self):
         frequence = 120
         frequence *= 1.6**self.y
         return frequence
 
+    def generateSamples(self):
+        t = np.linspace(0, self.duration, int(self.duration*self.rate))
+        frequences = np.repeat(self.pitches(), len(t)/len(self.y))
+        print(len(frequences), len(t))
+        
+        samples = np.sin(2 * np.pi * t * frequences)
+        return samples
+        
+
     def play(self):
         sample = self.toInt16()
         
-        play_obj = sa.play_buffer(sample, 2, 2, self.fs)
+        play_obj = sa.play_buffer(sample, 2, 2, self.rate)
 
         # Wait for playback to finish before exiting
         play_obj.wait_done()
@@ -32,7 +42,7 @@ class Sonification:
             
     def toInt16(self):
         # Ensure that highest value is in 16-bit range
-        sample = self.sample * 32767 / np.max(np.abs(self.sample))
+        sample = self.samples * 32767 / np.max(np.abs(self.samples))
 
         # Convert to 16-bit data
         sample = sample.astype(np.int16)
